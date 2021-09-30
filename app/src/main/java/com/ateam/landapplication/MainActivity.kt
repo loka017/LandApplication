064468cc8df3fa25eb6e5a795cbd79622d595e09
Loka017 <jessinthespace@gmail.com>
2021-10-01 06:10:25 +0900

    Initial commit

---
+++ app/src/main/java/com/ateam/landapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ateam.landapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ateam/landapplication/datas/Room.kt
@@ -0,0 +1,8 @@
+package com.ateam.landapplication.datas
+
+class Room(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String) {
+}
