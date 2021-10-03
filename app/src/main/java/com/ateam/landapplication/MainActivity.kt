a4091362d49e764a3a42af8dda10f006a9a3fa29
Loka017 <jessinthespace@gmail.com>
2021-10-03 20:54:40 +0900

    방의 기능으로 => 층수를 가공해서 String으로 주는 기능추가

--- app/src/main/java/com/ateam/landapplication/MainActivity.kt
+++ app/src/main/java/com/ateam/landapplication/MainActivity.kt
@@ -17,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mRoomList.add(Room(7500,"서울시 동대문구", 8, "서울시 동대문구 8층 7500만원 방입니다."))
-        mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 서대문구 8층 2억 4500만원 방입니다."))
-        mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 동대문구 5층 1억 4500만원 방입니다."))
-        mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 은평구 7층 3억 4500만원 방입니다."))
+        mRoomList.add(Room(24500,"서울시 서대문구", 0, "서울시 서대문구 8층 2억 4500만원 방입니다."))
+        mRoomList.add(Room(24500,"서울시 남대문구", 0, "서울시 남대문구 5층 1억 4500만원 방입니다."))
+        mRoomList.add(Room(24500,"서울시 은평구", 0, "서울시 은평구 7층 3억 4500만원 방입니다."))
         mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 서대문구 8층 2억 4500만원 방입니다."))
         mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 서대문구 8층 2억 4500만원 방입니다."))
         mRoomList.add(Room(24500,"서울시 동대문구", 0, "서울시 서대문구 8층 2억 4500만원 방입니다."))
--- app/src/main/java/com/ateam/landapplication/adapters/RoomAdapter.kt
+++ app/src/main/java/com/ateam/landapplication/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.ateam.landapplication.R
 import com.ateam.landapplication.datas.Room
 
@@ -22,6 +23,15 @@ class RoomAdapter(
         }
 
         val row = tempRow!!
+
+        val roomData = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        descriptionTxt.text = roomData.description
+        addressAndFloorTxt.text = roomData.address
 
         return row
     }
